7db95ff1f3f189c87d4e8072523a07efa4d0e88c
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-22 22:38:30 +0100

    :construction: Scaffold out necessary files and structure for map generation.

--- store/index.js
+++ store/index.js
@@ -1,15 +1,20 @@
-import { MAP } from '~/assets/json/map.example.json'
+// import map generator from js assets
+import map from '~/assets/js/map'
 
 export const state = () => ({
-  map: MAP
+  map: []
 })
 
 export const mutations = {
-
+  SET_MAP (state, map) { state.map = map }
 }
 
 export const actions = {
+  'map/generate' ({ commit }, { width, height }) {
+    const map = map.generate(width, height)
 
+    commit('SET_MAP')
+  }
 }
 
 export const getters = {
