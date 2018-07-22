125e552641fe5de047d240b6561b0d04ecd128aa
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-22 22:32:54 +0100

    :construction: Add bare minimum map display.

---
+++ store/index.js
@@ -0,0 +1,17 @@
+import { MAP } from '~/assets/json/map.example.json'
+
+export const state = () => ({
+  map: MAP
+})
+
+export const mutations = {
+
+}
+
+export const actions = {
+
+}
+
+export const getters = {
+
+}
