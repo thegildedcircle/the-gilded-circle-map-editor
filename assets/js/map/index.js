b6bd0973563812ae367c7b5c92d2c5053842a009
Trymunx <trymunx@gmail.com>
2018-07-23 10:39:13 +0100

    :bug: Remove hardcoded width and height

--- assets/js/map/index.js
+++ assets/js/map/index.js
@@ -3,7 +3,7 @@ import { base, biome } from './generate'
 
 export default {
   generate (W, H = W) {
-    const baseMap = base(10, 10)
+    const baseMap = base(W, H)
     const biomeMap = biome(baseMap)
 
     return biomeMap
